a6068a526bbf1f01a08195cf4c0bad4b72d7fae0
ninotevdorashvili <84516383+ninotevdorashvili@users.noreply.github.com>
2021-08-09 17:01:47 +0400

    ..

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ function users () {
         let ul = document.createElement('ul');
         responseData.data.forEach(x => {
             let li = document.createElement('li');
-            li.textContent = x.first_name + '' + x.last_name + '' + x.email;
-            ul.appendChild('li');
+            li.textContent = x.first_name + ' ' + x.last_name + ' ' + x.email;
+            ul.appendChild(li);
         });
-        document.getElementById('ajax').appendChild('ul');
+        document.getElementById('ajax').appendChild(ul);
     }
 
 
-    let request = new HMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.addEventListener('load', render);
     request.open('GET', 'https://reqres.in/api/users?page=2');
     request.send();
