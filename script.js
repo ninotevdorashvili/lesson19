4c37cb138638f051546df1c77b3e22450f6cca6f
ninotevdorashvili <84516383+ninotevdorashvili@users.noreply.github.com>
2021-08-09 05:02:40 +0400

    ..

---
+++ script.js
@@ -0,0 +1,34 @@
+// let navigation = document.getElementById('navi');
+// let burgerbar = document.getElementById('bar');
+// burgerbar.addEventListener('click', function(){
+//     navigation.classList.toggle('active');
+// });
+
+
+// ajax
+
+
+
+
+function users () {
+    function render () {
+        let response = this.responseText;
+        let responseData = JSON.parse(response);
+        console.log(responseData);
+        let ul = document.createElement('ul');
+        responseData.data.forEach(x => {
+            let li = document.createElement('li');
+            li.textContent = x.first_name + '' + x.last_name + '' + x.email;
+            ul.appendChild('li');
+        });
+        document.getElementById('ajax').appendChild('ul');
+    }
+
+
+    let request = new HMLHttpRequest();
+    request.addEventListener('load', render);
+    request.open('GET', 'https://reqres.in/api/users?page=2');
+    request.send();
+}
+
+users();
